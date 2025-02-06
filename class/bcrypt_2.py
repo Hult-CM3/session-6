4b8f01bf71ad0b876157c6fedd111dea4451c43f
C. Todd Lombardo <1851211+iamctodd@users.noreply.github.com>
2025-02-06 18:36:35 +0000

    ho

--- class/bcrypt_2.py
+++ class/bcrypt_2.py
@@ -8,14 +8,13 @@ print("\nEncoded password:", password.hex())
 salt = bcrypt.gensalt()
 print("\nGenerated salt:", salt)
 hashed_password = bcrypt.hashpw(password, salt)
-
 print("\nStored hashed password:", hashed_password)
 
-# Step 3: Simulating a login attempt
-attempt = input("\nEnter your password again to log in: ").encode()
-
-# Step 4: Check if the entered password matches the stored hash
-if bcrypt.checkpw(attempt, hashed_password):
-    print("✅ Login successful!")
-else:
-    print("❌ Incorrect password. Try again.")
+# Step 3: Simulating a login attempt and check if the entered password matches the stored hash
+while True:
+    attempt = input("\nEnter your password again to log in: ").encode()
+    if bcrypt.checkpw(attempt, hashed_password):
+        print("✅ Login successful!")
+        break
+    else:
+        print("❌ Incorrect password. Try again.")
