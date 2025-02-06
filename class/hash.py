c23c70bfe8d4a5dbf1e6a0cf02ea4d1c78278fa1
C. Todd Lombardo <1851211+iamctodd@users.noreply.github.com>
2025-02-06 19:22:21 +0000

    updates to hash and salt

--- class/hash.py
+++ class/hash.py
@@ -1,1 +1,21 @@
 import os
+import hashlib
+
+# Create a salt
+salt = os.urandom(16)  # Generate a random 16-byte salt
+
+# Set a password
+password = "mypassword123"
+
+# Hash the password with the salt
+hashed_password = hashlib.sha256(password.encode()).hexdigest()
+salted_and_hashed_password = salt.hex() + hashed_password
+
+# Print the results
+os.system('clear') # clears the terminal screen
+print("Entered password length:", len(password))
+print("\nSalt:", salt.hex())  # .hex() to convert salt to readable format
+print("\nHashed password:", hashed_password)
+print("\nHashed password length:", len(hashed_password))  # Check the length of the hashed password
+print("\nSalted & hashed password:", salted_and_hashed_password)  # Combine the salt and hashed password
+print("\nSalted & hashed password length:", len(salted_and_hashed_password))
--- class/salt.py
+++ class/salt.py
@@ -1,16 +1,20 @@
+# import libraries
 import random
 import string
 import hashlib
 
+# Make the salt
 salt_length = random.randint(5, 20)
-
 salt = ''.join(random.choices(string.ascii_letters + string.digits, k=salt_length))  # Generate a random salt
+
+# Set a password
 password = "mypassword123"
 
 # Hash the password with the salt
 hashed_password = hashlib.sha256(password.encode()).hexdigest()
 salted_and_hashed_password = salt + hashed_password
 
+# Print the results
 print("\nSalt:", salt)  # Convert salt to readable format
 print("\nHashed password:", hashed_password)
 print("\nHashed password length:", len(hashed_password))  # Check the length of the hashed password
