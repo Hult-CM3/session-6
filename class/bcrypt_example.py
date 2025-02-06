95558523363866f811e3233d30f50ac32573e7c4
C. Todd Lombardo <1851211+iamctodd@users.noreply.github.com>
2025-02-06 15:50:28 +0000

    bcrypt

--- class/bcrypt_example.py
+++ class/bcrypt_example.py
@@ -1,9 +1,10 @@
+# you must run "pip install bcrypt" in youur terminal before this will work
 import bcrypt
 
 password = input("please enter a pasword: ").encode()
 
 # Generate a salt and hash the password
-salt = bcrypt_example.gensalt()
-hashed_password = bcrypt_example.hashpw(password, salt)
+salt = bcrypt.gensalt()
+hashed_password = bcrypt.hashpw(password, salt)
 
 print("Salted & hashed password:", hashed_password)
